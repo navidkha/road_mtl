7d05e42757cc9669ec8b125a88bd7dc8e7ab3e2c
m.mousavi <mousavi@rahkar.co>
2021-05-01 22:57:21 +0430

    adding tasks

---
+++ road_mtl/tasks/taskInterface.py
@@ -0,0 +1,17 @@
+
+# import t
+
+class TaskInterface:
+
+    def encode(self, mat):
+        """
+        we need resnet.
+        create
+        """
+
+        resnet = bb.get_backbone(arch="resnet18", n_frames=args.SEQ_LEN)
+        clip.unsqueeze_(0)
+
+        print(resnet(clip))
+
+        pass
